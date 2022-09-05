095b1a6d67bd872a8cefda817e70d58b08211145
dudu <whqtkf12@naver.com>
2022-09-06 07:13:33 +0900

    feat: PromotionViewModel, PromotionView 구현

--- SwiftUI_OpenMarket/Presentation/Promotion/PromotionView.swift
+++ SwiftUI_OpenMarket/Presentation/Promotion/PromotionView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct PromotionView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @EnvironmentObject var viewFactory: ViewFactory
+  @ObservedObject var viewModel: PromotionViewModel
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      promotionBannerView
+      
+      Section {
+        hotItemsView
+      } header: {
+        hotItemsHeaderView
+      }
     }
+    .padding()
+    .navigationTitle("🎁 행사중")
+    .onAppear {
+      viewModel.requestProduct(10)
+    }
+  }
+}
+
+extension PromotionView {
+  var promotionBannerView: some View {
+    TabView {
+      ForEach(viewModel.promotions, id: \.name) { promotion in
+        Image(promotion.name)
+          .resizable()
+          .aspectRatio(1.0, contentMode: .fit)
+          .cornerRadius(20)
+          .onTapGesture {
+            viewModel.openURL(to: promotion.url)
+          }
+      }
+    }
+    .tabViewStyle(.page(indexDisplayMode: .always))
+    .frame(
+      width: UIScreen.main.bounds.size.width - 30,
+      height: UIScreen.main.bounds.size.width - 30
+    )
+    .padding()
+  }
+  
+  var hotItemsHeaderView: some View {
+    HStack {
+      Text("🚨 이 상품 놓치지 마세요!")
+        .font(.title2)
+        .fontWeight(.bold)
+        .padding(.leading)
+      Spacer()
+      NavigationLink("more ➡️") {
+        viewFactory.productMainView()
+      }
+      .tint(.black)
+    }
+  }
+  
+  var hotItemsView: some View {
+    let gridItems = [GridItem(.flexible(maximum: 200))]
+    
+    return ScrollView(.horizontal, showsIndicators: false) {
+      LazyHGrid(rows: gridItems) {
+        ForEach(viewModel.products) { product in
+          viewFactory.productGridView(with: product)
+        }
+      }
+    }
+  }
 }
 
 struct PromotionView_Previews: PreviewProvider {
-    static var previews: some View {
-        PromotionView()
+  static var previews: some View {
+    NavigationView {
+      ViewFactory.preview.promotionView()
+        .environmentObject(ViewFactory.preview)
     }
+  }
 }
