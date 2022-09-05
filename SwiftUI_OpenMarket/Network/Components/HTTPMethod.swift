caf0048f6ec0d28dd09157773800bdfb54ab6b29
dudu <whqtkf12@naver.com>
2022-09-05 15:11:49 +0900

    feat: Network 구성요소 구현
    - EndPoint Protocol
    - HTTPMethod 열거형
    - NetworkError 열거형

---
+++ SwiftUI_OpenMarket/Network/Components/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/05.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+  case get = "GET"
+  case post = "POST"
+  case patch = "PATCH"
+  case delete = "DELETE"
+}
