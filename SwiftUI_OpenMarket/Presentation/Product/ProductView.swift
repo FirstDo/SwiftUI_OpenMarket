1b5839f535e680f2dc015f844d690f1cabad08a7
dudu <whqtkf12@naver.com>
2022-09-06 01:38:23 +0900

    feat: ProductViewModel, ProductView 구현 (image, like 기능 제외)

---
+++ SwiftUI_OpenMarket/Presentation/Product/ProductView.swift
@@ -0,0 +1,65 @@
+//
+//  ProductView.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ProductView: View {
+  
+  @ObservedObject var viewModel: ProductViewModel
+  
+  var body: some View {
+    HStack(spacing: 16) {
+      Image(systemName: "swift")
+        .resizable()
+        .aspectRatio(1, contentMode: .fit)
+        .cornerRadius(20)
+        
+      informationView
+    }
+    .padding()
+  }
+}
+
+extension ProductView {
+  private var discountView: some View {
+    HStack {
+      Text(viewModel.discountPercentage)
+        .foregroundColor(.red)
+        .font(.caption)
+      Text(viewModel.price)
+        .foregroundColor(.gray)
+        .font(.caption)
+        .strikethrough()
+    }
+  }
+  
+  private var informationView: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      Text(viewModel.name)
+        .font(.title3)
+      
+      if viewModel.isSale {
+        discountView
+      }
+      
+      Text(viewModel.bargainPrice)
+      Spacer()
+      HStack {
+        Spacer()
+        Image(systemName: viewModel.isLike ? "star.fill" : "star")
+          .foregroundColor(.yellow)
+      }
+    }
+  }
+}
+
+struct ProductView_Previews: PreviewProvider {
+  static var previews: some View {
+    ProductView(viewModel: ProductViewModel(product: Product.preview))
+      .previewLayout(.fixed(width: 400, height: 130))
+  }
+}
