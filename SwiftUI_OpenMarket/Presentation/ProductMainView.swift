073a9aa7ba70550799a3c38c207ec6078219823b
dudu <whqtkf12@naver.com>
2022-09-06 04:34:56 +0900

    feat: ProductMainView, ProductMainViewModel 구현
    - 기본 UI구현
    - CellPrefatching
    - Infinity Scroll 구현

--- SwiftUI_OpenMarket/Presentation/ProductMainView.swift
+++ SwiftUI_OpenMarket/Presentation/ProductMainView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 
 struct ProductMainView: View {
   @EnvironmentObject var viewFactory: ViewFactory
+  @ObservedObject var viewModel: ProductMainViewModel
   
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    ScrollView {
+      productListView
+    }
+    .onAppear {
+      viewModel.request(0)
+    }
+    .navigationTitle("모든 상품")
+    .navigationBarTitleDisplayMode(.inline)
+  }
+}
+
+extension ProductMainView {
+  private var productListView: some View {
+    LazyVStack {
+      ForEach(Array(viewModel.items.enumerated()), id: \.element.id) { (index, product) in
+        viewFactory.productView(with: product)
+          .frame(height: 150, alignment: .center)
+          .onAppear {
+            viewModel.request(index)
+          }
+        Divider()
+          .background(.gray)
+      }
+    }
   }
 }
 
 struct ProductMainView_Previews: PreviewProvider {
   static var previews: some View {
-    ProductMainView()
-      .environmentObject(ViewFactory.preview)
+    NavigationView {
+      ViewFactory.preview.productMainView()
+        .environmentObject(ViewFactory.preview)
+    }
   }
 }
---
+++ SwiftUI_OpenMarket/Presentation/ProductMainViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ProductMainViewModel.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import Combine
+import Foundation
+
+final class ProductMainViewModel: ObservableObject {
+  private let productRepository: ProductRepository
+  private var cancellables = Set<AnyCancellable>()
+  private var page: Int = 1
+  
+  init(productRepository: ProductRepository) {
+    self.productRepository = productRepository
+  }
+  
+  private func requestProducts(page: Int, itemPerPage: Int) {
+    productRepository.requestProducts(page: page, itemPerPage: itemPerPage)
+      .receive(on: DispatchQueue.main)
+      .sink { completion in
+      
+      } receiveValue: { [weak self] products in
+        self?.items.append(contentsOf: products)
+      }
+      .store(in: &cancellables)
+  }
+
+  // MARK: - Input
+
+  func request(_ row: Int) {
+    if (row + 10) / 20 + 1 == page {
+      page += 1
+      requestProducts(page: page, itemPerPage: 20)
+    }
+  }
+  
+  // MARK: - OutPut
+  
+  @Published var items: [Product] = []
+}
