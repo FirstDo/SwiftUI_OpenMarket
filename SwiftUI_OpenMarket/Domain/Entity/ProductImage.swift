a0660e774bb19e01be058f6f5da0f929d4c2f7c8
dudu <whqtkf12@naver.com>
2022-09-05 14:36:07 +0900

    feat: Entity 구현
    - Product
    - ProductImage
    - Vendor

---
+++ SwiftUI_OpenMarket/Domain/Entity/ProductImage.swift
@@ -0,0 +1,16 @@
+//
+//  ProductImage.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/05.
+//
+
+import Foundation
+
+struct ProductImage {
+  let id: Int
+  let url: String
+  let thumbnailURL: String
+  let succeed: Bool
+  let issuedAt: String
+}
