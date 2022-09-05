1e6b9cd5c925ed964f5526d788470423a18a124c
dudu <whqtkf12@naver.com>
2022-09-06 05:07:14 +0900

    feat: 파일그룹화 및, PromotionView, ViewModel 생성

---
+++ SwiftUI_OpenMarket/Presentation/Promotion/PromotionView.swift
@@ -0,0 +1,20 @@
+//
+//  PromotionView.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import SwiftUI
+
+struct PromotionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PromotionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PromotionView()
+    }
+}
---
+++ SwiftUI_OpenMarket/Presentation/Promotion/PromotionViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PromotionViewModel.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import Foundation
+
+final class PromotionViewModel: ObservableObject {
+  
+}
