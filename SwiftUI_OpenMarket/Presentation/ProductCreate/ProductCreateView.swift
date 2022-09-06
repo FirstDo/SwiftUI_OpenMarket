fb1ffa106241d5746f2ce3145d90ac839f656aab
dudu <whqtkf12@naver.com>
2022-09-07 00:33:42 +0900

    feat: productCreateView / ViewModel 구현

--- SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateView.swift
+++ SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateView.swift
@@ -10,14 +10,93 @@ import SwiftUI
 struct ProductCreateView: View {
   @EnvironmentObject var viewFactory: ViewFactory
   @ObservedObject var viewModel: ProductCreateViewModel
+  @Environment(\.dismiss) var dismiss
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack(spacing: 30) {
+        productImagesView
+        userInputView
+      }
+    }
+    .padding()
+    .sheet(isPresented: $viewModel.showPhotoPickerView) {
+      PhotoPickerView(images: $viewModel.images)
+    }
+    .alert("에러 발생", isPresented: $viewModel.showAlertView) {
+      Button("확인") {}
+    } message: {
+      Text("잘못된 입력이 있습니다")
+    }
+    .onChange(of: viewModel.dismissView) { status in
+      if status {
+        dismiss()
+      }
+    }
+    .toolbar {
+      ToolbarItem(placement: .cancellationAction) {
+        Image(systemName: "xmark")
+          .onTapGesture {
+            viewModel.closeButtonDidTap()
+          }
+      }
+      
+      ToolbarItem(placement: .confirmationAction) {
+        Text("완료")
+          .onTapGesture {
+            viewModel.registerButtonDidTap()
+          }
+      }
+    }
+    .navigationTitle("상품 등록")
+    .navigationBarTitleDisplayMode(.inline)
+  }
+}
+
+extension ProductCreateView {
+  private var productImagesView: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      LazyHStack {
+        Image(systemName: "camera.viewfinder")
+          .resizable()
+          .aspectRatio(1.0, contentMode: .fit)
+          .frame(width: 100, height: 100)
+          .cornerRadius(10)
+          .onTapGesture {
+            viewModel.cameraImageDidTap()
+          }
+        
+        ForEach(Array(viewModel.images.enumerated()), id: \.offset) { index, image in
+          Image(uiImage: image)
+            .resizable()
+            .aspectRatio(1.0, contentMode: .fit)
+            .frame(width: 100, height: 100)
+            .cornerRadius(10)
+        }
+      }
+    }
+  }
+  
+  private var userInputView: some View {
+    VStack {
+      TextField("상품명", text: $viewModel.title)
+      Divider()
+      TextField("￦ 가격", text: $viewModel.price)
+      Divider()
+      TextField("할인금액 (가격보다 적어야 합니다)", text: $viewModel.discountPrice)
+      Divider()
+      TextField("재고수량", text: $viewModel.stock)
+      Divider()
+      TextEditor(text: $viewModel.description)
+    }
   }
 }
 
 struct ProductCreateView_Previews: PreviewProvider {
   static var previews: some View {
-    ViewFactory.preview.productCreateView()
-      .environmentObject(ViewFactory.preview)
+    NavigationView {
+      ViewFactory.preview.productCreateView()
+        .environmentObject(ViewFactory.preview)
+    }
   }
 }
--- SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateViewModel.swift
+++ SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by dudu on 2022/09/06.
 //
 
-import Foundation
+import UIKit
 
 final class ProductCreateViewModel: ObservableObject {
   private let productRepository: ProductRepository
@@ -13,4 +13,81 @@ final class ProductCreateViewModel: ObservableObject {
   init(productRepository: ProductRepository) {
     self.productRepository = productRepository
   }
+  
+  private var confirmTitle: Bool {
+    return !title.isEmpty
+  }
+  
+  private var confirmPrice: Bool {
+    guard let numberPrice = Int(price),
+          numberPrice >= 0, String(numberPrice) == price else {
+      return false
+    }
+    
+    return true
+  }
+  
+  private var confirmDiscountPrice: Bool {
+    guard let numberPrice = Int(price) else { return false }
+    guard let numberDiscountPrice = Int(discountPrice),
+          numberDiscountPrice >= 0 ,
+          String(numberDiscountPrice) == discountPrice,
+          numberPrice >= numberDiscountPrice else {
+      return false
+    }
+          
+    return true
+  }
+  
+  private var confirmStock: Bool {
+    guard let numberStock = Int(stock),
+          numberStock >= 0,
+          String(numberStock) == stock else {
+      return false
+    }
+    
+    return true
+  }
+  
+  private var confirmDescription: Bool {
+    return !description.isEmpty
+  }
+  
+  private var confirmInputs: Bool {
+    return confirmTitle && confirmPrice && confirmDiscountPrice && confirmStock && confirmDescription
+  }
+  
+  // MARK: - Input
+  
+  func registerButtonDidTap() {
+    if confirmInputs {
+      // TODO: register Product
+      dismissView = true
+    } else {
+      showAlertView = true
+    }
+  }
+  
+  func cameraImageDidTap() {
+    showPhotoPickerView = true
+  }
+  
+  func closeButtonDidTap() {
+    dismissView = true
+  }
+  
+  // MARK: - Output
+  
+  @Published var price: String = ""
+  @Published var title: String = ""
+  @Published var discountPrice: String = ""
+  @Published var stock: String = ""
+  @Published var description: String = ""
+  @Published var images: [UIImage] = []
+  
+  // MARK: - Routing
+
+  @Published var showPhotoPickerView: Bool = false
+  @Published var showAlertView: Bool = false
+  @Published var dismissView: Bool = false
 }
