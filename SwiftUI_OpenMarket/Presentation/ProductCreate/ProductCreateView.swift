57fee689d5b3d30929d86aeef59d34fa1412b471
dudu <whqtkf12@naver.com>
2022-09-06 18:51:26 +0900

    feat: ProductCreateView / ViewModel 생성

---
+++ SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductCreateView.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import SwiftUI
+
+struct ProductCreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductCreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCreateView()
+    }
+}
---
+++ SwiftUI_OpenMarket/Presentation/ProductCreate/ProductCreateViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCreateViewModel.swift
+//  SwiftUI_OpenMarket
+//
+//  Created by dudu on 2022/09/06.
+//
+
+import Foundation
+
+final class ProductCreateViewModel: ObservableObject {
+  
+}
